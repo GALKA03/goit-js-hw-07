908973c51137a6ed4c71ebb9a0456dc243d0851f
Galina Matvienko <galyagmgm@gmail.com>
2022-09-16 15:57:40 +0200

    3пы
    ыап

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,21 +4,20 @@ console.log(galleryItems);
 const imgConteiner = document.querySelector('ul');
 const galleryMarcup = createGalaryMarcup(galleryItems);
 console.log(imgConteiner)
-imgConteiner.insertAdjacentHTML('beforeend', galleryMarcup)
+imgConteiner.insertAdjacentHTML('beforeend',galleryMarcup)
 
-// //Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// /Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 function createGalaryMarcup(galleryItems){
     return galleryItems
     .map(({ preview, original, description }) => {
       return` 
       <a class="gallery__item" href="${ original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" title= ${description}/>
-</a>
-`}).join();
+  <img class="gallery__image"src="${preview}"alt="${description}" title= ${description}/>
+</a>`})
+.join();
 }
 const clickLinck = document.querySelector('.gallery__image')
 console.log(clickLinck.src)
-
 imgConteiner.addEventListener('click', onPicturesClick)
 function onPicturesClick(event){
   event.preventDefault();
@@ -30,35 +29,14 @@ function onPicturesClick(event){
     }
 console.log(noEventGallary)
 
-var lightbox = new SimpleLightbox('.gallery a', { animationSpeed:250 });
+var lightbox = new SimpleLightbox('.gallery a',{ animationSpeed:250 });
 console.log(lightbox)
     let gallery = new SimpleLightbox('.gallery a');
+    
     gallery.on('show.simplelightbox', function () {
         // do something…
     });
 }
-
-    //let lightbox = new SimpleLightbox('.gallery a', {captionDelay:250});
-    
-    //lightbox.on('shown.simplelightbox', function () {
-    //   console.log(lightbox)  
-
-
-    // });
-    //gallery.close;;
-//     let gallery = new SimpleLightbox('.gallery a',{docClose:true});
-// gallery.on('error.simplelightbox', function () {
-//     gallery.close// do something…
-// });
-    
-// ;
-   
-
-
-//     let gallery = $('.gallery a').simpleLightbox();
-// gallery.on('show.simplelightbox', function () {
-// 	// do something…
-// });
 
 
 
