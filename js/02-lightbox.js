7f567da37da3e91bfca4acd01882e018e45af97e
Galina Matvienko <galyagmgm@gmail.com>
2022-09-16 15:35:54 +0200

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,43 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 const imgConteiner = document.querySelector('ul');
-const galleryMarcup = createListGaleryMarcup(galleryItems);
-
- console.log(imgConteiner)
- imgConteiner.insertAdjacentHTML('beforeend', galleryMarcup)
- imgConteiner.addEventListener('click', onClick)
+const galleryMarcup = createGalaryMarcup(galleryItems);
+console.log(imgConteiner)
+imgConteiner.insertAdjacentHTML('beforeend', galleryMarcup)
 
 // //Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-function createListGaleryMarcup (galleryItems){
+function createGalaryMarcup(galleryItems){
     return galleryItems
-    .map(({ preview, original, description }) => { 
-    return ` 
-<li class ="galary__li">
-  <a class="gallery__item" href="${ original}">
-<img class="gallery__image" src="${preview}" alt="${description}" title ="${description}"/>
+    .map(({ preview, original, description }) => {
+      return` 
+      <a class="gallery__item" href="${ original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title= ${description}/>
 </a>
-<li>
-`})
+`}).join();
+}
+const clickLinck = document.querySelector('.gallery__image')
+console.log(clickLinck.src)
+
+imgConteiner.addEventListener('click', onPicturesClick)
+function onPicturesClick(event){
+  event.preventDefault();
+
+ const noEventGallary = event.target.nodeName;
+     //если действие происходит не в нашем диве == ничего не происходит 
+  if (noEventGallary !== 'IMG'){
+        return;
+    }
+console.log(noEventGallary)
+
+var lightbox = new SimpleLightbox('.gallery a', { animationSpeed:250 });
+console.log(lightbox)
+    let gallery = new SimpleLightbox('.gallery a');
+    gallery.on('show.simplelightbox', function () {
+        // do something…
+    });
 }
 
-
-const clickLinck = document.querySelector('.gallery__image')
-console.log(clickLinck.href)
-clickLinck.addEventListener('click',onClick);
-
-function onClick (event){
-    event.preventDefault();
-    const clickFunctions = event.target.nodeName;
- 
-    if(clickFunctions !== 'IMG'){
-        return
-        
-    }
-    let lightbox = new SimpleLightbox('.gallery a', {captionDelay:250});
+    //let lightbox = new SimpleLightbox('.gallery a', {captionDelay:250});
     
-    lightbox.on('shown.simplelightbox', function () {
-      console.log(lightbox)  
-    });
+    //lightbox.on('shown.simplelightbox', function () {
+    //   console.log(lightbox)  
+
+
+    // });
     //gallery.close;;
 //     let gallery = new SimpleLightbox('.gallery a',{docClose:true});
 // gallery.on('error.simplelightbox', function () {
@@ -54,4 +60,7 @@ function onClick (event){
 // 	// do something…
 // });
 
-}
+
+
+
+
